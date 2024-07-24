f0b77e06d2bfaed698a942ea47ea7c522b847f47
PedroBVidal <pedropopa98@gmail.com>
2024-07-24 17:39:25 -0300

    added bounding function

--- trab2.py
+++ trab2.py
@@ -1,9 +1,9 @@
 import sys 
-
+import argparse
 
 opt_x = ""
 opt_p = sys.maxsize
-
+cont_nodes = 0
 def generate_combinations(bits, current, x, l, current_set, array):
     
     if len(current_set) == l:
@@ -23,19 +23,46 @@ def generate_combinations(bits, current, x, l, current_set, array):
         print("formed sequence current_set", current_set)
         return
 
-    b = greddy_heuristic(array, current, current_set, l)
-    
+    b = 0  
+    if (args.a):
+        b = current.count('1') + 1
+    else:
+        b = current.count('1') + bounding_function(array, current, current_set,l)
+
+  
     for bit in bits:
+        global cont_nodes
+        len_current_set = len(current_set)
         if (bit == "1"):
             current_set = list(set(current_set + array[len(current)])) 
+        
+        len_current_set_after = len(current_set)
         print("current ", current)
         print("current set ", current_set)
-        len_current_set = current.count('1');
         print("opt_p \n ", opt_p)
-        if b >= opt_p:
-            return 
-        if len_current_set < opt_p: 
-            generate_combinations(bits, current + bit, x, l,current_set, array)
+
+        generate_bit1_branch = True 
+        if (not(args.f)): 
+            if (len_current_set == len_current_set_after and bit == '1'):
+                generate_bit1_branch = False
+                
+
+        len_current = current.count('1') 
+
+        if (args.o):
+            if (generate_bit1_branch == True):
+                cont_nodes += 1
+                generate_combinations(bits, current + bit ,x, l,current_set, array)
+        elif (args.a):
+            if b < opt_p: 
+                if (generate_bit1_branch == True):
+                    cont_nodes += 1
+                    generate_combinations(bits, current + bit ,x, l,current_set, array)
+        else:
+            if b < opt_p: 
+                if (generate_bit1_branch == True):
+                    cont_nodes += 1
+                    generate_combinations(bits, current + bit, x, l,current_set, array)
 
 # greddy heuristic
 def greddy_heuristic(array, current, current_set,l):
@@ -48,7 +75,7 @@ def greddy_heuristic(array, current, current_set,l):
 
     array_temp.sort(key=len, reverse=True)
     
-    len_current_set = current.count('1');
+    len_current_set = current.count('1')
 
     it = 0
     for i in range(len(array_temp)):
@@ -57,6 +84,34 @@ def greddy_heuristic(array, current, current_set,l):
         if len(current_set) == l:
            return len_current_set + it 
     return sys.maxsize 
+
+def bounding_function(array, current, current_set,l):
+    groups_left = l - len(current_set)
+    candidates_left_with_groups_filtered = list()
+   
+    for i in range(len(current),len(array)):
+        result = [item for item in array[i] if item not in current_set] 
+        candidates_left_with_groups_filtered.append(result)
+       
+
+    candidates_left_with_groups_filtered.sort(key=len, reverse=True)
+    
+    print("candidates left with gropus filtered", candidates_left_with_groups_filtered)
+
+    cont_unique_groups = 0
+    cont_necessary_candidates = 0
+
+    for i in range(len(candidates_left_with_groups_filtered)):
+        cont_unique_groups += len(candidates_left_with_groups_filtered[cont_necessary_candidates]) 
+        cont_necessary_candidates += 1
+        if cont_unique_groups >= groups_left:
+           break
+
+    print("cont necessary candidates-------------------------------------", cont_necessary_candidates)
+    
+    return cont_necessary_candidates
+
+         
 
 input_data = sys.stdin.readline()
 numbers  = input_data.split()
@@ -82,9 +137,19 @@ for i in range(n):
 # Definindo os bits e o comprimento desejado
 bits = "01"
 x = n
-
-
 current_set = list()
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Process some command-line options.")
+
+# Define the command-line arguments
+parser.add_argument('-a', '--a', action='store_true', help='função limitante default')
+parser.add_argument('-o', '--o', action='store_true', help='desligar cortes por otimalidade')
+parser.add_argument('-f', '--f', action='store_true', help='deligar coretes por viabilidade')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
 
 # Chamando a função recursiva
 stdout = generate_combinations(bits, "", x,l,current_set,array)
@@ -101,7 +166,7 @@ else:
         if opt_x[i] == '1':
             print(i+1, end=" ");
 
-print(" ");
+print("cont nos ", cont_nodes);
 
 
 
